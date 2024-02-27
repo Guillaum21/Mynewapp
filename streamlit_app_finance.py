1a3e47df1af50b51e666a95831e3274da0c7556a
Guillaum21 <161482866+Guillaum21@users.noreply.github.com>
2024-02-27 16:13:26 +0100

    Update streamlit_app_finance.py

--- streamlit_app_finance.py
+++ streamlit_app_finance.py
@@ -2,14 +2,23 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 
-st.write("""
-#STOCK PRICE: GOOGLE
-""")
 
 tickerSymbol = 'GOOGL'
-tickerData = yt.Ticker(tickerSymbol)
+
+
+tickerData = yf.Ticker(tickerSymbol)
+
+
+st.write("""
+# Stock Price and Volume: Google (GOOGL)
+""")
+
+
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+# Open High Low Close Volume Dividends Stock Splits
+
 
 st.line_chart(tickerDf.Close)
+
+
 st.line_chart(tickerDf.Volume)
